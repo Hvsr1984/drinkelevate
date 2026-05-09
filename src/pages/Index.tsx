5fd5f1e66ee24ccb4a3817f45cc837207f25b5e1
Hvsr1984 <2025pietcsharshvardhan063@poornima.org>
2026-05-09 01:31:11 +0000

    Changes
    Co-authored-by: Hvsr1984 <250857506+Hvsr1984@users.noreply.github.com>

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -2,14 +2,8 @@ import { Navbar } from "@/components/Navbar";
 import { PromoBanner } from "@/components/PromoBanner";
 import { HeroSection } from "@/components/HeroSection";
 import { WaveDivider } from "@/components/WaveDivider";
-import { WhyElevateSection } from "@/components/WhyElevateSection";
-import { BottleShowcaseSection } from "@/components/BottleShowcaseSection";
 import { ProductsSection } from "@/components/ProductsSection";
-import { LifestyleSection } from "@/components/LifestyleSection";
 import { AboutSection } from "@/components/AboutSection";
-import { TrustSection } from "@/components/TrustSection";
-import { DistributorSection } from "@/components/DistributorSection";
-import { InstagramSection } from "@/components/InstagramSection";
 import { Footer } from "@/components/Footer";
 
 const Index = () => {
@@ -19,15 +13,8 @@ const Index = () => {
       <PromoBanner />
       <HeroSection />
       <WaveDivider />
-      <WhyElevateSection />
-      <BottleShowcaseSection />
       <ProductsSection />
-      <WaveDivider flip />
-      <LifestyleSection />
       <AboutSection />
-      <TrustSection />
-      <DistributorSection />
-      <InstagramSection />
       <Footer />
     </div>
   );
